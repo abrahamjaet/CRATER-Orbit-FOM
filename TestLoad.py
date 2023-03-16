a01c46c08b3a93b63922496c8e237061fa5ddfd7
mark-walvoord <114098855+mark-walvoord@users.noreply.github.com>
2023-03-16 11:57:47 -0600

    CSV file containing sphere from FOM

---
+++ TestLoad.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import SphereSave
+import time
+#from csltk.utilities import System
+import math
+from numpy import savetxt
+
+coordinates = np.genfromtxt('FOMLoad.csv', delimiter=',')
